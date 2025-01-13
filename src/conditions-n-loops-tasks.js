fa52a6c8c13899a8ee5706affddc1bfadc6a5e96
Nikita Maneev <nick@Nikitas-iMac.local>
2025-01-13 15:36:04 -0700

    third commit

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -249,8 +249,11 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) return i;
+  }
+  return -1;
 }
 
 /**
@@ -268,8 +271,13 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let newNum = num;
+  while (newNum > 0) {
+    if (newNum % 10 === digit) return true;
+    newNum = Math.floor(newNum / 10);
+  }
+  return false;
 }
 
 /**
@@ -285,8 +293,23 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let sum1 = 0;
+  let sum2 = 0;
+  if (arr.length >= 3) {
+    for (let i = 1; i < arr.length - 1; i += 1) {
+      for (let y = 0; y < i; y += 1) {
+        sum1 += arr[y];
+      }
+      for (let x = i + 1; x < arr.length; x += 1) {
+        sum2 += arr[x];
+      }
+      if (sum1 === sum2) return i;
+      sum1 = 0;
+      sum2 = 0;
+    }
+  }
+  return -1;
 }
 
 /**
